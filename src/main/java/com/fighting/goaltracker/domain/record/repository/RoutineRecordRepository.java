b785aad4012b2e6d514c3d632aef03b586eacd70
shunni12 <113586874+shunni12@users.noreply.github.com>
2026-06-05 11:04:35 +0900

    Refactor repository methods for user and routine IDs

--- src/main/java/com/fighting/goaltracker/domain/record/repository/RoutineRecordRepository.java
+++ src/main/java/com/fighting/goaltracker/domain/record/repository/RoutineRecordRepository.java
@@ -11,9 +11,7 @@ import java.util.Optional;
 @Repository
 public interface RoutineRecordRepository extends JpaRepository<RoutineRecord, Integer> {
 
-    // 특정 유저의 특정 날짜 루틴 기록 전체 조회
-    List<RoutineRecord> findByUserIdAndRecordDate(Integer userId, LocalDate recordDate);
+    List<RoutineRecord> findByUserUserIdAndRecordDate(Integer userId, LocalDate recordDate);
 
-    // 특정 루틴이 특정 날짜에 이미 체크되었는지 확인하기 위한 용도
-    Optional<RoutineRecord> findByRoutineIdAndRecordDate(Integer routineId, LocalDate recordDate);
+    Optional<RoutineRecord> findByRoutineRoutineIdAndRecordDate(Integer routineId, LocalDate recordDate);
 }
