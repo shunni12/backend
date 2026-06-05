f98a7dd7de61966adc46225e0098a51c50ee9b34
shunni12 <113586874+shunni12@users.noreply.github.com>
2026-06-05 11:06:36 +0900

    Refactor RoutineRepository to use userUserId

--- src/main/java/com/fighting/goaltracker/domain/routine/repository/RoutineRepository.java
+++ src/main/java/com/fighting/goaltracker/domain/routine/repository/RoutineRepository.java
@@ -12,10 +12,8 @@ import java.util.List;
 @Repository
 public interface RoutineRepository extends JpaRepository<Routine, Integer> {
 
-    // 특정 유저의 모든 루틴 조회
-    List<Routine> findByUserId(Integer userId);
+    List<Routine> findByUserUserId(Integer userId);
 
-    // 💡 1:N 컬렉션 테이블(routine_repeat_days)에서 오늘 요일(DayOfWeek)과 일치하고 활성화된 루틴 조회
-    @Query("SELECT r FROM Routine r JOIN r.repeatDays d WHERE r.user.id = :userId AND r.isActive = 'true' AND d = :dayOfWeek")
+    @Query("SELECT r FROM Routine r JOIN r.repeatDays d WHERE r.user.userId = :userId AND r.isActive = 'true' AND d = :dayOfWeek")
     List<Routine> findActiveRoutinesByDay(@Param("userId") Integer userId, @Param("dayOfWeek") DayOfWeek dayOfWeek);
 }
